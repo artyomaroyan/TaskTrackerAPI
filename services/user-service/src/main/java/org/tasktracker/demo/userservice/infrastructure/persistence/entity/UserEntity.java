3c3dbb1cf9e4f24da9cc5795fc9f79934ea1d8c0
Artyom <aroyanart98@gmail.com>
2025-11-02 23:29:04 +0400

    feat(user-service): create `UserEntity` class.

---
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/infrastructure/persistence/entity/UserEntity.java
@@ -0,0 +1,29 @@
+package org.tasktracker.demo.userservice.infrastructure.persistence.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static lombok.AccessLevel.PRIVATE;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 01.11.25
+ * Time: 13:21:33
+ */
+@Getter
+@Builder
+@Table("users")
+@NoArgsConstructor(access = PRIVATE)
+@AllArgsConstructor(access = PRIVATE)
+public class UserEntity {
+    @Id
+    private UUID id;
+    private String name;
+    private String email;
+    private String role;
+    private Instant createdAt;
+}
