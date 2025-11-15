c3e319be786925cb082688bd89938155fd9d52d5
Artyom <aroyanart98@gmail.com>
2025-11-15 21:52:00 +0400

    feat(user-service): core logic. - added UserService interface. - added UserRegistrationService interface.

---
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/service/UserRegistrationService.java
@@ -0,0 +1,9 @@
+package org.tasktracker.demo.userservice.application.service;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 15.11.25
+ * Time: 21:47:31
+ */
+public interface UserRegistrationService {
+}
