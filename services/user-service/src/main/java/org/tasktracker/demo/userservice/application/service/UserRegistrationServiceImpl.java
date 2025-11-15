233efb2022e4291bf640ef09a8bdb600a02f93ca
Artyom <aroyanart98@gmail.com>
2025-11-15 21:54:05 +0400

    feat(user-service): core logic. - added UserServiceImpl class which implements UserService interface. - added UserRegistrationServiceImpl class which implements UserRegistrationService interface.

---
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/service/UserRegistrationServiceImpl.java
@@ -0,0 +1,12 @@
+package org.tasktracker.demo.userservice.application.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 15.11.25
+ * Time: 21:52:49
+ */
+@Service
+public class UserRegistrationServiceImpl implements UserRegistrationService {
+}
