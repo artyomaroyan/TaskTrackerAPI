a4870bf39e968482dfb65036d83e0b925b39823d
Artyom <aroyanart98@gmail.com>
2025-11-18 23:13:30 +0400

    modify(user-service): - delete `UserResponse` record.

--- services/user-service/src/main/java/org/tasktracker/demo/userservice/application/dto/UserResponse.java
+++
@@ -1,9 +0,0 @@
-package org.tasktracker.demo.userservice.application.dto;
-
-/**
- * Author: Artyom Aroyan
- * Date: 15.11.25
- * Time: 21:57:36
- */
-public record UserResponse(boolean registered, String message) {
-}
