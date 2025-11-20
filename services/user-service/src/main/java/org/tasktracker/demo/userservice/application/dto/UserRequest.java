68ac3e228910311c17bb8df7d163953d53299603
Artyom <aroyanart98@gmail.com>
2025-11-20 12:54:35 +0400

    refactor(user-service): - add `@Schema` annotation to request fields.

--- services/user-service/src/main/java/org/tasktracker/demo/userservice/application/dto/UserRequest.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package org.tasktracker.demo.userservice.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.validation.annotation.Validated;
@@ -12,11 +13,14 @@ import org.tasktracker.demo.userservice.domain.model.Email;
  */
 @Validated
 public record UserRequest(
+        @Schema(example = "user1")
         @NotBlank(message = "username is required field!")
         @Size(message = "username must be between 8 - 20 characters", min = 8, max = 20)
         String username,
+        @Schema(example = "user1@gmail.com")
         @NotBlank(message = "email is required field!")
         Email email,
+        @Schema(example = "password1")
         @NotBlank(message = "password is required field!")
         @Size(message = "password must be between 8 - 20 characters", min = 8, max = 20)
         String password
