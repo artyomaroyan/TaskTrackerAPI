12353aa0cc3d536f6f25f1fe44b5097f819d33fb
Artyom <aroyanart98@gmail.com>
2025-11-21 01:56:38 +0400

    feat(user-service): - add flyway migration file. - add global exception handler controller.

---
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/controller/ExceptionHandlerController.java
@@ -0,0 +1,33 @@
+package org.tasktracker.demo.userservice.application.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 20.11.25
+ * Time: 13:24:05
+ */
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<Map<String, String>> handleArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors
+                        .put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getReason());
+    }
+}
---
+++ services/user-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,8 @@
+create table if not exists users(
+    id uuid primary key not null default gen_random_uuid(),
+    username varchar(20) not null unique ,
+    password varchar(500) not null ,
+    email varchar(50) not null unique ,
+    role varchar(20) not null ,
+    created_at timestamp default current_timestamp
+);
