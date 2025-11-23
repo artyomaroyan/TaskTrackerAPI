8ee88f46d7503c042d7b10e1e49d458a7c6e9797
Artyom <aroyanart98@gmail.com>
2025-11-23 23:22:05 +0400

    refactor(user-service) modify user service. - add and implement `findUserById` method. - add and implement `findUserByUsername` method. - add and implement `deleteUserById` method.

--- services/user-service/src/main/java/org/tasktracker/demo/userservice/application/service/UserService.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/service/UserService.java
@@ -1,9 +1,19 @@
 package org.tasktracker.demo.userservice.application.service;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.tasktracker.demo.userservice.domain.model.User;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
 /**
  * Author: Artyom Aroyan
  * Date: 15.11.25
  * Time: 21:45:27
  */
 public interface UserService {
+    Mono<User> findUserById(@NotNull UUID id);
+    Mono<User> findUserByUsername(@NotBlank String username);
+    Mono<Void> deleteUserById(@NotNull UUID id);
 }
--- services/user-service/src/main/java/org/tasktracker/demo/userservice/application/service/UserServiceImpl.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/service/UserServiceImpl.java
@@ -1,12 +1,45 @@
 package org.tasktracker.demo.userservice.application.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.tasktracker.demo.userservice.domain.model.User;
+import org.tasktracker.demo.userservice.domain.repository.UserRepository;
+import org.tasktracker.demo.userservice.exception.UserNotFoundException;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 /**
  * Author: Artyom Aroyan
  * Date: 15.11.25
  * Time: 21:52:24
  */
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public Mono<User> findUserById(UUID id) {
+        log.debug("finding user by ID: {}", id);
+        return userRepository.findById(id)
+                .switchIfEmpty(Mono.error(new UserNotFoundException("User with id " + id + " not found")));
+    }
+
+    @Override
+    public Mono<User> findUserByUsername(String username) {
+        log.debug("Finding user by username: {}", username);
+        return userRepository.findByUsername(username)
+                .switchIfEmpty(Mono.error(new UserNotFoundException("User with username " + username + " not found")));
+    }
+
+    @Override
+    public Mono<Void> deleteUserById(UUID id) {
+        log.debug("Deleting user by ID: {}", id);
+        return this.findUserById(id)
+                .then(userRepository.delete(id))
+                .doOnSuccess(_ -> log.info("Successfully deleted user with ID: {}", id));
+    }
 }
