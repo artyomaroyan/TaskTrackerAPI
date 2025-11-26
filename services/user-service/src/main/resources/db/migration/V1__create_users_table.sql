867d0da2e8626abd8f9b9ae20f33e9748ffacfc8
Artyom <aroyanart98@gmail.com>
2025-11-27 00:09:47 +0400

    fix(user-service) fixing fields type. - add messing fields in db migration file.

--- services/user-service/src/main/resources/db/migration/V1__create_users_table.sql
+++ services/user-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -4,5 +4,7 @@ create table if not exists users(
     password varchar(500) not null ,
     email varchar(50) not null unique ,
     role varchar(20) not null ,
-    created_at timestamp default current_timestamp
+    authorities varchar(100) not null ,
+    created_at timestamp default current_timestamp,
+    active boolean not null default true
 );
