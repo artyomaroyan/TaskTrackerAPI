e884348ddd4442bd6535cf6feecf8a88d4d2be5b
Artyom <aroyanart98@gmail.com>
2025-12-03 15:26:40 +0400

    refactor(user-service): Authorization. - add `@PreAuthorize` annotation and set roles for methods.

--- services/user-service/src/main/java/org/tasktracker/demo/userservice/application/usecases/UserServiceImpl.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/usecases/UserServiceImpl.java
@@ -2,6 +2,7 @@ package org.tasktracker.demo.userservice.application.usecases;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.tasktracker.demo.userservice.application.ports.in.UserService;
 import org.tasktracker.demo.userservice.domain.model.User;
@@ -23,6 +24,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
+    @PreAuthorize("hasRole('ADMIN') or @reactiveSecurityService.isSelfOrAdmin(id)")
     public Mono<User> findUserById(UUID id) {
         log.debug("finding user by ID: {}", id);
         return userRepository.findById(id)
@@ -30,6 +32,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @PreAuthorize("hasRole('USER') or #username = authentication.principal.username")
     public Mono<User> findUserByUsername(String username) {
         log.debug("Finding user by username: {}", username);
         return userRepository.findByUsername(username)
@@ -37,6 +40,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN') or hasAuthority('DELETE')")
     public Mono<Void> deleteUserById(UUID id) {
         log.debug("Deleting user by ID: {}", id);
         return this.findUserById(id)
