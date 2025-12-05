974f3c0f5b6b584f38af3d7b71af3baf5c57c01d
Artyom <aroyanart98@gmail.com>
2025-12-05 23:28:01 +0400

    refactor(user-service): mapper. - accept model changes in `UserMapper` class.

--- services/user-service/src/main/java/org/tasktracker/demo/userservice/application/mapper/UserMapper.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/mapper/UserMapper.java
@@ -9,10 +9,6 @@ import org.tasktracker.demo.userservice.domain.model.Role;
 import org.tasktracker.demo.userservice.domain.model.User;
 import org.tasktracker.demo.userservice.infrastructure.persistence.entity.UserEntity;
 
-import java.util.Set;
-
-import static org.tasktracker.demo.userservice.domain.model.Authorities.*;
-
 /**
  * Author: Artyom Aroyan
  * Date: 21.11.25
@@ -31,12 +27,11 @@ public class UserMapper extends BaseMapper<User, UserEntity> {
                     entity.getPassword(),
                     new Email(entity.getEmail()),
                     Role.valueOf(entity.getRole()),
-                    Set.of(CREATE.name(), READ.name(), UPDATE.name(), DELETE.name()),
                     entity.getCreatedAt(),
                     entity.isActive());
         } catch (IllegalArgumentException ex) {
             log.warn("Invalid role value: {}", entity.getRole(), ex);
-            throw new DataMappingException("Invalid role value" + entity.getRole(), ex.getCause());
+            throw new DataMappingException("Invalid role value " + entity.getRole(), ex.getCause());
         }
     }
 
@@ -48,7 +43,6 @@ public class UserMapper extends BaseMapper<User, UserEntity> {
                 .password(domain.getPassword())
                 .email(domain.getEmail().value())
                 .role(domain.getRole().name())
-                .authorities(domain.getAuthorities())
                 .createdAt(domain.getCreatedAt())
                 .active(domain.isActive())
                 .build();
