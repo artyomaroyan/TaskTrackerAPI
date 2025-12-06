176bc67ba671d4ce345b3df9bddf33acf71626c7
Artyom <aroyanart98@gmail.com>
2025-12-06 15:54:36 +0400

    refactor(user-service): temporary comment `UserDetailsProviderImpl` class and `UserDetailsProvider` interface.

--- services/user-service/src/main/java/org/tasktracker/demo/userservice/application/ports/out/UserDetailsProvider.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/ports/out/UserDetailsProvider.java
@@ -1,3 +1,4 @@
+/*
 package org.tasktracker.demo.userservice.application.ports.out;
 
 import jakarta.validation.constraints.NotNull;
@@ -8,8 +9,9 @@ import reactor.core.publisher.Mono;
  * Author: Artyom Aroyan
  * Date: 25.11.25
  * Time: 14:32:12
- */
+
 public interface UserDetailsProvider {
     Mono<UserDetails> findByUsername(@NotNull String username);
     Mono<Boolean> validCredentials(@NotNull String username, @NotNull String password);
 }
+ */
