3fd9df42007222f1e4cf0cd466b688c2ceda7881
Artyom <aroyanart98@gmail.com>
2025-12-07 18:55:48 +0400

    feat(user-service): add `UserResponse` record.

---
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/dto/UserResponse.java
@@ -0,0 +1,19 @@
+package org.tasktracker.demo.userservice.application.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 07.12.25
+ * Time: 18:38:51
+ */
+public record UserResponse(
+        UUID id,
+        String username,
+        String email,
+        String role,
+        Instant createdAt,
+        boolean active
+) {
+}
