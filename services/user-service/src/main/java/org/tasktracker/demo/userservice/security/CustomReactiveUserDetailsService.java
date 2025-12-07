07658c2bed6bf483924ca3726b8d81929bd6599e
Artyom <aroyanart98@gmail.com>
2025-12-07 19:00:55 +0400

    refactor(user-service): architecture modification. - integrate `UserDetailsProviderImpl` to `CustomReactiveUserDetailsService` class. - improve Role checking logic.

--- services/user-service/src/main/java/org/tasktracker/demo/userservice/security/CustomReactiveUserDetailsService.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/security/CustomReactiveUserDetailsService.java
@@ -2,16 +2,13 @@ package org.tasktracker.demo.userservice.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.tasktracker.demo.userservice.application.ports.out.UserRepository;
 import reactor.core.publisher.Mono;
-
-import java.util.stream.Collectors;
 
 /**
  * Author: Artyom Aroyan
@@ -23,24 +20,32 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CustomReactiveUserDetailsService implements ReactiveUserDetailsService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Mono<UserDetails> findByUsername(String username) {
         log.debug("Looking for user with username: {}", username);
+
         return userRepository.findByUsername(username)
-                .doOnNext(user -> log.debug("Found user: {} with username: {}", user.getUsername(), username))
                 .switchIfEmpty(Mono.defer(() -> {
                     log.debug("No user found with username: {}", username);
                     return Mono.error(new UsernameNotFoundException("User not found: " + username));
                 }))
                 .map(user -> {
-                    log.debug("Building UserDetails for: {}", user.getUsername());
-                    return User.withUsername(user.getUsername())
-                            .password(user.getPassword())
-                            .authorities(user.getAuthorities().stream()
-                                    .map(SimpleGrantedAuthority::new)
-                                    .collect(Collectors.toSet()))
-                            .build();
+                    log.debug("Found user: {}", user.getUsername());
+
+                    return new UserIdentity(
+                            user.getUsername(),
+                            user.getPassword(),
+                            user.getAuthorities(),
+                            user.isActive()
+                    );
                 });
+    }
+
+    public Mono<Boolean> validCredentials(String username, String rawPassword) {
+        return findByUsername(username)
+                .map(user -> passwordEncoder.matches(rawPassword, user.getPassword()))
+                .defaultIfEmpty(false);
     }
 }
