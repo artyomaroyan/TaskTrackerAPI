3aca19fe51a6edae87be726df3459d09e0b85ee0
Artyom <aroyanart98@gmail.com>
2025-12-13 00:45:29 +0400

    refactor(user-service): code cleanup and improvement. - change `role` field return type in `UserIdentity` record. - cleanup `@PreAuthorize` annotation in `UserServiceImpl` class. - delete unnecessary methods from `TokenProvider` interface. - accept changes in `JwtTokenProvider` class.

--- services/user-service/src/main/java/org/tasktracker/demo/userservice/security/JwtTokenProvider.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/security/JwtTokenProvider.java
@@ -1,18 +1,18 @@
 package org.tasktracker.demo.userservice.security;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.tasktracker.demo.userservice.application.exception.TokenValidationException;
 import org.tasktracker.demo.userservice.application.ports.out.KeyProvider;
 import org.tasktracker.demo.userservice.application.ports.out.TokenProvider;
 import org.tasktracker.demo.userservice.infrastructure.configuration.JwtProperties;
 import reactor.core.publisher.Mono;
 
-import java.util.*;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Author: Artyom Aroyan
@@ -45,7 +45,8 @@ public class JwtTokenProvider implements TokenProvider {
                     "iat", now,
                     "exp", exp,
                     "iss", jwtProperties.issuer(),
-                    "authorities", user.authorities()
+                    "role", user.roles(),
+                    "auth", user.authorities()
             );
 
             return Jwts.builder()
@@ -53,58 +54,16 @@ public class JwtTokenProvider implements TokenProvider {
                     .claims(claims)
                     .signWith(keyProvider.getPrivateKey(), Jwts.SIG.RS256)
                     .compact();
-
-            // I like this type of token creating,
-            // but I think creating separate claims and set them is more effective and fast version.
-//            return Jwts.builder()
-//                    .id(UUID.randomUUID().toString())
-//                    .subject(user.username())
-//                    .issuedAt(now)
-//                    .expiration(exp)
-//                    .issuer(jwtProperties.issuer())
-//                    .claim("authorities", user.authorities())
-//                    .claim("test", "this is testing claim to show that you can add claims as you want.")
-//                    .signWith(keyProvider.getPrivateKey(), Jwts.SIG.RS256)
-//                    .compact();
         }).doOnError(error -> log.error("Token generation failed for user {}", user.username(), error));
     }
 
     @Override
-    public Mono<UserIdentity> extractUserIdentity(String token) {
+    public Mono<Boolean> validateToken(String token) {
         return Mono.fromCallable(() -> {
             Claims claims = jwtParser.parseSignedClaims(token)
                     .getPayload();
 
-            String username = claims.getSubject();
-            @SuppressWarnings("unchecked")
-            List<String> authorityStrings = claims.get("authorities", List.class);
-
-            if (username == null || username.isBlank())
-                throw new TokenValidationException("Token subject (username) is missing!");
-
-            Set<String> authorities = new HashSet<>(authorityStrings);
-
-            return new UserIdentity(
-                    username,
-                    null,
-                    authorities,
-                    true
-            );
-        }).onErrorMap(JwtException.class, ex ->
-                new TokenValidationException("Invalid token: " + ex.getMessage()));
-    }
-
-    @Override
-    public Mono<Boolean> validateToken(String token) {
-        return extractUserIdentity(token)
-                .map(_ -> true)
-                .onErrorReturn(false);
-    }
-
-    @Override
-    public Mono<Boolean> isTokenExpired(String token) {
-        return extractUserIdentity(token)
-                .map(_ -> false)// If we can extract, it's not expired
-                .onErrorResume(TokenValidationException.class, _ -> Mono.just(true));
+            return !claims.getExpiration().before(new Date());
+        });
     }
 }
--- services/user-service/src/main/java/org/tasktracker/demo/userservice/security/UserIdentity.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/security/UserIdentity.java
@@ -3,8 +3,10 @@ package org.tasktracker.demo.userservice.security;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.tasktracker.demo.userservice.domain.model.Role;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,8 +18,17 @@ import java.util.stream.Collectors;
 public record UserIdentity(
         String username,
         String password,
+        Set<Role> roles,
         Set<String> authorities,
         boolean enabled) implements UserDetails {
+
+    public UserIdentity(String username, String password, Set<Role> roles, Set<String> authorities, boolean enabled) {
+        this.username = Objects.requireNonNull(username);
+        this.password = password;
+        this.roles = Objects.requireNonNull(roles);
+        this.authorities = Objects.requireNonNull(authorities);
+        this.enabled = enabled;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
