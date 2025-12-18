1f41606415e04c86ac6a87d051a87fbebb63bfb0
Artyom <aroyanart98@gmail.com>
2025-12-18 17:54:36 +0400

    refactor(task-service): add `Status` and `Priority` enums.

--- services/task-service/src/main/java/org/tasktracker/demo/taskservice/domain/model/Priority.java
+++ services/task-service/src/main/java/org/tasktracker/demo/taskservice/domain/model/Priority.java
@@ -1,10 +1,12 @@
 package org.tasktracker.demo.taskservice.domain.model;
 
-
-
 /**
  * Author: Artyom Aroyan
  * Date: 21.11.25
  * Time: 21:36:06
- */public enum Priority {
+ */
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
 }
