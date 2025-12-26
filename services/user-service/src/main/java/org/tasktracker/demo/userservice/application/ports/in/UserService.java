5bd7a03589cbee300aa2e47970dea7100baabc9d
Artyom <aroyanart98@gmail.com>
2025-12-26 21:40:46 +0400

    refactor(user-service): code cleanup. - delete unused `UserDetailsProvider` interface. - delete unused import in `UserService` interface.

--- services/user-service/src/main/java/org/tasktracker/demo/userservice/application/ports/in/UserService.java
+++ services/user-service/src/main/java/org/tasktracker/demo/userservice/application/ports/in/UserService.java
@@ -3,7 +3,6 @@ package org.tasktracker.demo.userservice.application.ports.in;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.tasktracker.demo.userservice.application.dto.UserResponse;
-import org.tasktracker.demo.userservice.domain.model.User;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
